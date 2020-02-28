a512d510b6c76098f2a769cbcbccc8899c8d308a
Ccile <cecilisse@gmail.com>
2020-02-28 14:50:08 +0100

    [DEV] criticDisplay : modify expected html file for empty directory analysis for the corresponding test

--- src/main/ReadJSONClass.java
+++ src/main/ReadJSONClass.java
@@ -1,18 +1,14 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.apache.commons.io.IOUtils;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
 import java.io.*;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class ReadJSONClass {
 
-    public static void ReadJSON(String fileURL) throws IOException, ParseException, EmptyDirectoryAnalysisException, NotAFileException {
+    public static void ReadJSON(String fileURL) throws IOException, NotAFileException {
 
         File JSONToBeRead = new File(fileURL);
 
@@ -20,7 +16,7 @@ public class ReadJSONClass {
             throw new FileNotFoundException();
         }
 
-        if (JSONToBeRead.isDirectory()){
+        if (JSONToBeRead.isDirectory()) {
             throw new NotAFileException("This is a folder !");
         }
 
@@ -28,45 +24,68 @@ public class ReadJSONClass {
 
         String directoryPath = JSONToBeRead.getPath();
         int endOfPath = directoryPath.lastIndexOf("/");
-        String outputPath = directoryPath.substring(0, endOfPath+1);
+        String outputPath = directoryPath.substring(0, endOfPath + 1);
+
         writeHtmlFile(htmlContent, outputPath);
     }
 
-    private static String readJSONWriteHtmlContent(File JSONToBeRead){
+    private static String readJSONWriteHtmlContent(File JSONToBeRead) {
 
-        String JSONContent ="";
-        String val="";
+        String htmlContent = "";
+        String path = "";
+        String type = "";
+        String score = "";
+        ArrayList content = new ArrayList();
 
         try {
+            FileReader jsonFileReader = new FileReader(JSONToBeRead.getPath());
             ObjectMapper oMapper = new ObjectMapper();
-            HashMap tempObject = oMapper.readValue(new FileReader(JSONToBeRead.getPath()),
+            HashMap<String, Object> criticFileMap;
 
-                    new TypeReference<HashMap<String, Object>>() {
-
-                    });
-
-            val = (String)tempObject.get("score");
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            if (jsonFileReader != null) {
+                criticFileMap = oMapper.readValue(jsonFileReader, new TypeReference<>() {
+                });
+            } else {
+                throw new ParseException("Empty file", 0);
+            }
+            path = (String) criticFileMap.get("path");
+            type = (String) criticFileMap.get("type");
+            score = (String) criticFileMap.get("score");
+            content = (ArrayList) criticFileMap.get("content");
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
-        if (val.equals("0")){
-            JSONContent = "{\n" +
-                    "\t\"path\" : \"test/samples/EmptyRepository\",\n" +
-                    "\t\"type\" : \"directory\",\n" +
-                    "\t\"score\" : \"0\",\n" +
-                    "\t\"content\" : [\n" +
-                    "\t]\n" +
-                    "}\n";
+        if (score.equals("0")) {
+            htmlContent = "<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<head>\n" +
+                    "\t<meta charset=\"utf-8\">\n" +
+                    "\t<!--<link rel=\"stylesheet\" href=\"critic.css\">-->\n" +
+                    "\t<title>Critic</title>\n" +
+                    "</head>\n" +
+                    "\n" +
+                    "<body>\n" +
+                    "\n" +
+                    "\t<div>\n" +
+                    "\t\t<table>\n" +
+                    "\t\t\t<tbody>\n" +
+                    "\t\t\t\t<tr>\n" +
+                    "\t\t\t\t\t<td><img src=\"../image/folderIcon.jpg\" alt=\"folder icon\" height=\"40px\"></td>\n" +
+                    "\t\t\t\t\t<td width=\"400px\">test/samples/EmptyRepository</td>\n" +
+                    "\t\t\t\t\t<td>0</td>\n" +
+                    "\t\t\t\t</tr>\n" +
+                    "\t\t\t</tbody>\n" +
+                    "\t\t</table>\n" +
+                    "\t</div>\n" +
+                    "\n" +
+                    "\n" +
+                    "</body>\n" +
+                    "</html>\n";
         }
-
-        String HTMLContent = "<p>" + JSONContent + "</p>\n";
-
-
-        return HTMLContent;
+        return htmlContent;
     }
 
     private static void writeHtmlFile(String htmlContent, String path) throws IOException {
